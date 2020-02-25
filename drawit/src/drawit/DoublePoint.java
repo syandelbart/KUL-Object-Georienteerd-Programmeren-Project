4ad7f0ac14b5ccfeb68bdb10b06d706ef572b74e
EliasDeCoppel <elias.decoppel@student.kuleuven.be>
2020-02-25 15:20:22 +0100

    DoublePoint finished(i think)

--- drawit/src/drawit/DoublePoint.java
+++ drawit/src/drawit/DoublePoint.java
@@ -1,5 +1,36 @@
 package drawit;
 
 public class DoublePoint {
-
+	private double x;
+	private double y;
+	
+	
+	public DoublePoint(double x,double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+	
+	public DoublePoint plus(DoubleVector other) {
+		DoublePoint result = new DoublePoint(other.x + this.x,other.y + this.y);
+		return result;
+	}
+	
+	public DoubleVector minus(DoublePoint other) {
+		DoubleVector result = new DoubleVector(other.x - this.x,other.y - this.y);
+		return result;
+	}
+	
+	public IntPoint round() {
+		IntPoint result = new IntPoint((int) Math.round(this.x),(int)Math.round(this.y));
+		return result;
+	}
 }
+
--- drawit/src/drawit/IntVector.java
+++ drawit/src/drawit/IntVector.java
@@ -1,5 +1,11 @@
 package drawit;
 
 public class IntVector {
-
+	private int x;
+	private int y;
+	
+	public IntVector(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 }
