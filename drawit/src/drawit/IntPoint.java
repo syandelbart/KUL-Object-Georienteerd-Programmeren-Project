6fda5ffa6a45f8e032c825bb09a160ad99291995
Syan Delbart <syantjee@gmail.com>
2020-02-25 08:18:33 +0100

    Init commit

---
+++ drawit/src/drawit/IntPoint.java
@@ -0,0 +1,18 @@
+package drawit;
+
+public class IntPoint {
+	private int x,y;
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public IntPoint(int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
