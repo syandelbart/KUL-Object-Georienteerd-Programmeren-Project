38b33666712e8e1acb3a4468e0894b102020a9f5
EliasDeCoppel <elias.decoppel@student.kuleuven.be>
2020-02-25 23:40:14 +0100

    IntVector finished

--- drawit/src/drawit/IntVector.java
+++ drawit/src/drawit/IntVector.java
@@ -8,4 +8,35 @@ public class IntVector {
 		this.x = x;
 		this.y = y;
 	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public long crossProduct(IntVector other) {
+		return (long)getX() * other.getY() - (long)getY() * other.getX();
+	}
+	
+	public boolean isCollinearWith(IntVector other) {
+		if (this.crossProduct(other) == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public long dotProduct(IntVector other) {
+		return (long)getX() * other.getX() + (long)getY() * other.getY();
+	}
+	
+	public DoubleVector asDoubleVector() {
+		DoubleVector result = new DoubleVector(this.x,this.y);
+		return result;
+	}
 }
+
