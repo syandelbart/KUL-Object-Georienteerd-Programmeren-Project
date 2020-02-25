3aaaa096196bec9905fb8324c999615df1f84980
EliasDeCoppel <elias.decoppel@student.kuleuven.be>
2020-02-25 23:32:53 +0100

    InPoint finished
    last function is really complicated so needs to be checked.

--- drawit/src/drawit/IntPoint.java
+++ drawit/src/drawit/IntPoint.java
@@ -32,6 +32,38 @@ public class IntPoint {
 	}
 	
 	public boolean isOnLineSegment(IntPoint b,IntPoint c) {
-		
+		int rico = (c.y - b.y)/(c.x - b.x);
+		if(this.equals(b) || this.equals(c)) {
+			return false;
+		}
+		if(this.y-b.y == rico*(this.x - b.x)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public DoublePoint asDoublePoint() {
+		DoublePoint result = new DoublePoint(this.getX(),this.getY());
+		return result;
+	}
+	
+	public IntPoint plus(IntVector vector) {
+		IntPoint result = new IntPoint(this.x + vector.x, this.y + vector.y);
+		return result;
+	}
+	
+	public static boolean lineSegmentsIntersect(IntPoint a,IntPoint b,IntPoint c,IntPoint d) {
+		DoubleVector newVectorac = new DoubleVector(a.x-c.x,a.y-c.y);
+		DoubleVector newVectorab = new DoubleVector(a.x-b.x,a.y-b.y);
+		DoubleVector newVectorad = new DoubleVector(a.x-d.x,a.y-d.y);
+		if (Math.signum(newVectorac.crossProduct(newVectorab)) * Math.signum(newVectorad.crossProduct(newVectorab)) < 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
+
--- drawit/src/drawit/PointArrays.java
+++ drawit/src/drawit/PointArrays.java
@@ -1,5 +1,5 @@
 package drawit;
 
 public class PointArrays {
-
+	
 }
