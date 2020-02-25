0bc18094bb18701e9bf557c63a2a99cefd0cb4ea
EliasDeCoppel <elias.decoppel@student.kuleuven.be>
2020-02-26 00:18:44 +0100

    started with PointArrays but i'm having some issues.

--- drawit/src/drawit/PointArrays.java
+++ drawit/src/drawit/PointArrays.java
@@ -1,5 +1,31 @@
 package drawit;
 
 public class PointArrays {
-	
+	public static String checkDefinesProperPolygon(IntPoint[] points) {
+		for(int i = 0; i < points.length; i++) {
+			int a = i;
+			int b = i+1;
+			if (b > points.length - 1) {
+				b = b - points.length;
+			}
+			int c = i+2;
+			if (c > points.length - 1) {
+				c = c - points.length;
+			}
+			int d = i+3;
+			if (d > points.length - 1) {
+				d = d - points.length;
+			}
+			if (lineSegmentsIntersect(points[a],points[b],points[c],points[d]) == true) {
+				return "2 lines intersect";
+			}
+			for(int j = 0; j < points.length; j++) {
+				if(j != i && points[i].getX() == points[j].getX() && points[i].getY() == points[j].getY()) {
+					return "2 points are the same";
+				}
+			}
+		}
+		return null;
+	}
 }
+
