f171fc7ea2568e4bc967ddfc24c3cb9b327e5c44
EliasDeCoppel <elias.decoppel@student.kuleuven.be>
2020-02-25 14:49:14 +0100

    lol

--- drawit/src/drawit/IntPoint.java
+++ drawit/src/drawit/IntPoint.java
@@ -1,7 +1,13 @@
 package drawit;
 
 public class IntPoint {
-	private int x,y;
+	private int x;
+	private int y;
+	
+	public IntPoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 	
 	public int getX() {
 		return this.x;
@@ -11,8 +17,21 @@ public class IntPoint {
 		return this.y;
 	}
 	
-	public IntPoint(int x,int y) {
-		this.x = x;
-		this.y = y;
+	public boolean equals(IntPoint other) {
+		if(other.x == this.x && other.y == this.y) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public IntVector minus(IntPoint other) {
+		IntVector result = new IntVector(this.getX()-other.getX(),this.getY()-other.getY());
+		return result;
+	}
+	
+	public boolean isOnLineSegment(IntPoint b,IntPoint c) {
+		
 	}
 }
