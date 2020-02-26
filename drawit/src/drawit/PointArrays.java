fbda4fd11bb7cbea9dde9cb0d9a8000905122226
EliasDeCoppel <elias.decoppel@student.kuleuven.be>
2020-02-26 18:50:58 +0100

    finished PointArrays

--- drawit/src/drawit/PointArrays.java
+++ drawit/src/drawit/PointArrays.java
@@ -30,5 +30,44 @@ public class PointArrays {
 		}
 		return null;
 	}
+	
+	public static IntPoint[] copy(IntPoint[] points) {
+		IntPoint[] result = new IntPoint[points.length];
+		for(int i = 0; i < points.length; i++) {
+			result[i] = points[i];
+		}
+		return result;
+	}
+	
+	public static IntPoint[] insert(IntPoint[] points,int index,IntPoint point) {
+		IntPoint[] result = new IntPoint[points.length+1];
+		for(int i = 0; i < index; i++) {
+			result[i] = points[i];
+		}
+		result[index] = point;
+		for(int i = index+1; i < result.length; i++) {
+			result[i] = points[i-1];
+		}
+		return result;
+	}
+	
+	public static IntPoint[] remove(IntPoint[] points,int index) {
+		IntPoint[] result = new IntPoint[points.length-1];
+		for(int i = 0; i < index; i++) {
+			result[i] = points[i];
+		}
+		for(int i = index; i < result.length; i++) {
+			result[i] = points[i+1];
+		}
+		return result;
+	}
+	
+	public static IntPoint[] update(IntPoint[] points,int index,IntPoint value) {
+		IntPoint[] result = PointArrays.copy(points);
+		PointArrays.remove(points, index);
+		PointArrays.insert(points, index, value);
+		return result;
+	}
 }
+
 
